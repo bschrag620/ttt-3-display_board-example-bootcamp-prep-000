6bc45bc20a3db45d98442098214a2c387f03f1b3
Brad Schrag <brad.schrag@gmail.com>
2018-07-20 17:56:17 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,6 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
-put "   |   |   "
-put "-----------"
-put "   |   |   "
-put "-----------"
-put "   |   |   "
+puts "   |   |   "
+puts "-----------"
+puts "   |   |   "
+puts "-----------"
+puts "   |   |   "
